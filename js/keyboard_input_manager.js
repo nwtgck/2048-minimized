3b4a260ae91ccfa9847f0e51563bdeb02834c6c6
Ryo Ota <nwtgck@gmail.com>
2018-10-17 21:33:21 +0900

    [remove] Unsupport vim key bind and W,D,S,A keys

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -39,15 +39,7 @@ class KeyboardInputManager {
       38: 0, // Up
       39: 1, // Right
       40: 2, // Down
-      37: 3, // Left
-      75: 0, // Vim up
-      76: 1, // Vim right
-      74: 2, // Vim down
-      72: 3, // Vim left
-      87: 0, // W
-      68: 1, // D
-      83: 2, // S
-      65: 3  // A
+      37: 3  // Left
     };
 
     // Respond to direction keys
